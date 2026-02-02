bf032ff0174b89cdf9a7d38b51087f02ace08aa2
Griz394 <grizzlymine394@gmail.com>
2026-02-02 23:14:39 +0400

    Дизайн основной страницы
    Допилил дизайн окон регистрации и авторизации , а также скрипты к ним, сделал дизайн основной страницы

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,8 +4,8 @@ async function Login()
 {
     const username_box = document.getElementById('username_box');
     const password_box = document.getElementById('password_box');
-    const username = username_box.value.trim();
-    const password = password_box.value.trim();
+    let username = username_box.value.trim();
+    let password = password_box.value.trim();
 
     if(!username || !password){
         alert('Введите имя пользователя и пароль!');
@@ -13,9 +13,9 @@ async function Login()
     }
     const url = API_URL+`/Auth?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
     try{
-        const response = await fetch(url);
-        const result = await response.json();
-        if(result == 1){
+        let response = await fetch(url);
+        let result = await response.json();
+        if(result === 1){
             window.location.href = 'Main.html';
         }
         else{
@@ -30,8 +30,8 @@ async function Registration()
 {
     const username_box = document.getElementById('username_box');
     const password_box = document.getElementById('password_box');
-    const username = username_box.value.trim();
-    const password = password_box.value.trim();
+    let username = username_box.value.trim();
+    let password = password_box.value.trim();
 
     if(!username || !password){
         alert('Введите имя пользователя и пароль!');
@@ -40,9 +40,9 @@ async function Registration()
 
     const url = API_URL + `/Reg?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
     try{
-        const response = await fetch(url);
-        const result = await response.json();
-        if (result ==1){
+        let response = await fetch(url);
+        let result = await response.json();
+        if (result === 1){
             alert('Аккаунт успешно создан!');
             window.location.href = 'Index.html';
         }
